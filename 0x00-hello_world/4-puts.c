6de60a058f205464d05d8bd3dcc77b12c6e0035e
Lmdingi <lmdingi4@gmail.com>
2022-06-06 08:20:29 -0700

    lkj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main (void) {
-        char p[] = "\"Programming is like building a multilingual puzzle\n";
+        char p[] = "\"Programming is like building a multilingual puzzle";
 	puts(p);
 	return 0;
 }
