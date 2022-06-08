ec960480db11b0d7a12aed8572a2a1078fc4ce71
Lmdingi <lmdingi4@gmail.com>
2022-06-08 14:32:16 -0700

    j

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,25 @@
 #include "main.h"
-#include "_putchar.h"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main (void)
+
+int main(void)
 {
 	print_alphabet();
 	return (0);
 }
 
-/** int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-*/
+
+
+
+
+
+
+
+
+
 void print_alphabet(void)
 {
 	char a;
