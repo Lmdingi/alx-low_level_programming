b6edd1f4aee2d467f389781f058598a97cce0da5
Lmdingi <lmdingi4@gmail.com>
2022-06-11 07:41:11 -0700

    good

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * maint - print small alphabet and capital letters
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char small = 'a', big = 'A';
+
+	if (small <= 'z')
+	{
+		while (small <= 'z')
+		{
+			putchar(small);
+			small++;
+		}
+	}
+	if (big <= 'Z')
+	{
+		while (big <= 'Z')
+		{
+			putchar(big);
+			big++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
