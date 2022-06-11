9cfffb2cf9f4ebbb9eec5a01f91274d0d5cb5712
Lmdingi <lmdingi4@gmail.com>
2022-06-11 08:25:42 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print base numbers
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int base_number = 0;
+
+	while (base_number <= 10)
+	{
+		printf("%d", base_number);
+		base_number++;
+	}
+	printf("\n");
+	return (0);
+}
