a53c061a3ae99115e5d37dc4a900385dfe09deeb
Lmdingi <lmdingi4@gmail.com>
2022-06-12 03:50:28 -0700

    print 10x alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet only
+ * @c: takes in a character
+ * Return: 0 or 1 to identify type of character
+ */
+int _isalpha(int c)
+{
+	int r;
+
+	if (c <= 'a' && c <= 'z')
+	{
+		r = 1;
+	}
+	else if (c <= 'A' && 'Z')
+	{
+		r = 1;
+	}
+	else
+	{
+		r = 0;
+	}
+	return (r);
+}
