2584a49d24cc196e9b64ac4858cd4db79483c1ba
Lmdingi <lmdingi4@gmail.com>
2022-06-12 08:10:30 -0700

    print 10x alphabet

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_last_digit - print last number
  * @a: take digits
- * Return: last number
+ *i Return: last number
  */
 int print_last_digit(int a)
 {
-	int r;
-	r = a % 10;
-	return (r);
+
+	_putchar((a % 10) + '0');
+	return (a);
 }
