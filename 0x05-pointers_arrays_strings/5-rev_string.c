b003baf5174e13239cfd7ce333b501283d518171
Lmdingi <lmdingi4@gmail.com>
2022-06-13 11:58:30 -0700

    hard

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: take pointer string in
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+	c--;
+	while (c > 0)
+	{
+		_putchar(s[c]);
+		c--;
+	}
+	_putchar('\n');
+}
