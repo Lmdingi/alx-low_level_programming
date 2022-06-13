ed2d0fd7da2bd3b3f00944e01456fbefb1c42124
Lmdingi <lmdingi4@gmail.com>
2022-06-13 12:56:57 -0700

    please work

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,17 @@ void puts2(char *str)
 
 	for (c = 0; *(str + c) != '\0'; c++)
 	{
-		_putchar(*(str + c));
-		if (*(str + c) != '\0')
+		if ((*str + c) == '\0')
 		{
-			c++;
+			break;
+		}
+		else
+		{
+			_putchar(*(str + c));
+			if (*(str + c) != '\0')
+			{
+				c++;
+			}
 		}
 	}
 	_putchar('\n');
