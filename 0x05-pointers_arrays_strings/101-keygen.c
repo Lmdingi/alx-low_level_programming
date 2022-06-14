e563c1fee513cf4591855643734116057642da89
Lmdingi <lmdingi4@gmail.com>
2022-06-14 07:51:00 -0700

    print all

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,14 +8,14 @@ int main(void)
 	srand((unsigned int)(time(NULL)));
 	int i;
 	char pass[12];
-	
+	char capLetter, letter;
 	
 	for (i = 0; i < 4; i++)
 	{
 		pass[i] = rand() % 9;
-		char capLetter = 'A' + (rand() % 26);
+		capLetter = 'A' + (rand() % 26);
 		pass[i + 2] = capLetter;
-		char letter = 'a' + (rand() % 26);
+		letter = 'a' + (rand() % 26);
 		pass[i + 3] = letter;
 		printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
 	}
