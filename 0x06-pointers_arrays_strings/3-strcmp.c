85f1d323469afd1ede8521c79798bd137cefe9dd
Lmdingi <lmdingi4@gmail.com>
2022-06-17 05:36:16 -0700

    gg

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,33 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int a = 0, b = 0, c = 0, r = 0, lim;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (s1[a])
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+		b++;
 	}
-	return (0);
+	if (a <= b)
+	{
+		lim = a;
+	}
+	else
+	{
+		lim = b;
+	}
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+		c++;
+	}
+	return (r);
+}
