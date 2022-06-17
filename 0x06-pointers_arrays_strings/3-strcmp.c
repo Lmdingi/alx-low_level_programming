3a3cb0c598fbcf8d11150620cf5198af3e6b8add
Lmdingi <lmdingi4@gmail.com>
2022-06-16 20:41:33 -0700

    jjhhj

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int c = 0, at = 0, st1 = 0, st2 = 0;
-	
-	for (c = 0; s1[c] != '\0'; c++)
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		st1 = st1 + s1[c];
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	for (c = 0; s2[c] != '\0'; c++)
-	{
-		st2 = st2 + s2[c];
-	}
-	at = st1 - st2;
-	return (at);
-}
+	return (0);
