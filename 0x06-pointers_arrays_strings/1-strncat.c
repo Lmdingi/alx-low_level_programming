a9927862da7a2604fda3fad95fe104f3fce65a64
Lmdingi <lmdingi4@gmail.com>
2022-06-16 20:54:10 -0700

    jjhhj

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,17 +9,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, co;
-	
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
-	for (co = 0; co < n; co++)
-	{
-		dest[i + co] = src[co];
-		if (src[co] == '\0')
-			co = n;
-	}
-	return (dest);
+	int dlen = 0, j = 0;
 
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+	while (j < n && src[j])
+	{
+		dest[dlen] = src[j];
+		dlen++;
+		j++;
+	}
+	dest[dlen + n + 1] = '\0';
+	return (dest);
 }
