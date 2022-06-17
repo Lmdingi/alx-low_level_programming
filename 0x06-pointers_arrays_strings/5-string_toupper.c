2c8e0f2895a1e104434361c3e2064b6169323057
Lmdingi <lmdingi4@gmail.com>
2022-06-17 05:24:18 -0700

    gg

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * string_toupper - Changes lowercase
+ * @p: Take string
+ * Return: char
+ */
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+		a++;
+	}
+	return (p);
+}
