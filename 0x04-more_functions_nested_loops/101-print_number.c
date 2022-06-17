16bc48aec2263f2b26586b2630fb38aecefd3a15
Lmdingi <lmdingi4@gmail.com>
2022-06-16 20:16:35 -0700

    check

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_number - take number
+ * @n: take number
+ * Return: void
+ */
+int print_number(int n)
+{
+	int i;
+	i = n;
+	_putchar(i + '0');
+	_putchar('\n');
+	return (0);
+}
